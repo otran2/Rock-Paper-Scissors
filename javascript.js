85b103de11604b41f1466947e45dd8a671f3a290
Oscar Tran <oscartran007@gmail.com>
2025-08-24 02:10:26 -0700

    Create function for random computer hand Created getComputerChoice(), a function which returns a random string in the set of "rock" "paper" and "scissors" which is used to represent the computer's choice.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+function getComputerChoice(){
+    let rand = Math.floor(Math.random() * 3)
+    if (rand === 0){
+        return "rock"
+    }
+    else if (rand === 1){
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
