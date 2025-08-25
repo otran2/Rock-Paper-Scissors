a0f1e9d1556d4fda1e95ccff79ea09efd1ed46e9
Oscar Tran <oscartran007@gmail.com>
2025-08-25 00:13:21 -0700

    Add function to get player hand input Added the getHumanChoice function which prompts the user for their hand in rock paper scissors and returns what they enter.

--- javascript.js
+++ javascript.js
@@ -11,4 +11,26 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    let hand = prompt("What hand will you choose? (Please enter one of the following 3 options: Rock, Paper, or Scissors)");
+
+    if (hand === null){
+        return null;
+    }
+    else if (hand.toLowerCase() === "rock") {
+        return "rock";
+    } else if (hand.toLowerCase() === "paper") {
+        return "paper";
+    } else if (hand.toLowerCase() === "scissors") {
+        return "scissors";
+    } else {
+        return "";
+    }
+}
+
+const playButton = document.querySelector(".play-button");
+const log = document.querySelector("#log");
+// playButton.addEventListener("click", getHumanChoice);
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
